787d25df7928491d83d7df03a0f12930a58e2e61
SoYeonKim522 <sykim096@gmail.com>
2021-11-13 21:35:35 +0900

    week1 ~step 12

--- week 1-Jetpack Compose basics/week1/app/src/main/java/com/example/composecodelabweek1/MainActivity.kt
+++ week 1-Jetpack Compose basics/week1/app/src/main/java/com/example/composecodelabweek1/MainActivity.kt
@@ -1,40 +1,63 @@
 package com.example.composecodelabweek1
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.shapes
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composecodelabweek1.ui.theme.ComposeCodelabWeek1Theme
+
+
+//******** What's new : Lazy Column, rememberSavable, animation
+//if activity restarted -> all state is lost
+//remember : only as long as the composable is kept in the Composition
+//rememberSavable : save each state surviving configuration changes (such as rotations) and process death.
+//
+//animate~AsState 로 만들어진 애니메이션
+//it is interruptible
+//if the target value changes, animate~AsState restarts the animation and points to new value
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {  //xml 대신 setContent 안에 레이아웃 설정
-            ComposeCodelabWeek1Theme {  // 프로젝트명+Theme 으로 자동으로 설정됨
-                // A surface container using the 'background' color from the theme
+            ComposeCodelabWeek1Theme {  // 프로젝트명+Theme 으로 테마 이름이 자동으로 설정되고
+                //이 안에 있는 함수는 해당 테마의 UI가 적용됨 (Theme.kt에서 확인 가능)
                 MyApp()
             }
         }
     }
 }
 
+
 //화면 이동 구현 : onboarding screen -> greetings screen
 @Composable
 fun MyApp() {
-    var shouldShowOnBoarding by remember { mutableStateOf(true) }  //hoist the state
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }  //***rememberSavable 로 대체 -> 화면음 rotate 되어도 onboarding screen 으로 돌아오지 않음
+
     if (shouldShowOnBoarding){
         OnBoardingScreen(onContinueClicked = {shouldShowOnBoarding = false})  //콜백함수 -> 상태 변경시키기 -> 화면 전환
     } else {
         Greetings()
     }
 }
-
 
 
 @Composable
@@ -54,15 +77,15 @@ fun OnBoardingScreen(onContinueClicked: () -> Unit) {  // callback -> we can mut
             ) {
                 Text(text = "Continue")
             }
-
         }
     }
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 20.dp)) {  //전체 레이아웃의 위아래 패딩
-        for (name in names){
+//set the size and fill list with the value contained in its lambda
+private fun Greetings(names: List<String> = List(100) {"$it"}) {   //it => list index
+    LazyColumn(modifier = Modifier.padding(vertical = 20.dp)) {
+        items(items = names) { name ->    //items import needed
             Greeting(name = name)
         }
     }
@@ -78,34 +101,49 @@ fun OnboardingPreview() {  //다른 이름으로 preview
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }  // = 로 remember -> 항상 expanded.value 로 refer 해야함
-    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    var expanded by remember { mutableStateOf(false) }  //by 키워드로 remember (property delegate) -> 뒤에 value 붙일 필요 x
+
+    val extraPadding by animateDpAsState(       //expanded 되었을 때만 생기도록 만든 extraPadding 변수에 by로 animate 추가
+        if(expanded) 48.dp else 0.dp,
+        //세부 애니메이션 설정
+        animationSpec = spring(   //spring spec 은 duration 등 시 설정은 안됨
+            dampingRatio = Spring.DampingRatioMediumBouncy,  //바운스 효과
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)  //각 아이템의 세로 가로 마진
-    ) {  //Surface => background. 배경색을 primary로 바꾸면 Material이 폰트색 자동으로 onPrimary(흰색)로 바꿔줌
+    ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)) {
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {  //애니메이션 설정 후 padding 최소값 0 으로 지정!! (이거 안하면 앱 크래시)
                 Text(text = "Hello")
-                Text(text = name)
+                Text(text = name,
+                    style = MaterialTheme.typography.h5.copy(   //copy -> you can modify a predefined style
+                        fontWeight = FontWeight.ExtraBold
+                    ))
             }
             //버튼
             OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
 
     }
 }
 
-
-//하나의 파일에 다른 이름을 가진 여러 Preview 가능
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Preview(  //add annotation to the same function -> 프리뷰가 추가됨!
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320, heightDp = 720)
 @Composable
 fun DefaultPreview() {
     ComposeCodelabWeek1Theme {
--- week 1-Jetpack Compose basics/week1/app/src/main/java/com/example/composecodelabweek1/ui/theme/Color.kt
+++ week 1-Jetpack Compose basics/week1/app/src/main/java/com/example/composecodelabweek1/ui/theme/Color.kt
@@ -2,7 +2,15 @@ package com.example.composecodelabweek1.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//기본색상 리스트
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+//색상 추가 (Theme.kt 에서도 수정 필요)
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)
+val White = Color(0xFFFFFFFF)
