d3abf3ed9b39fa449fced40c2787926e20ab08ae
SoYeonKim522 <sykim096@gmail.com>
2021-11-14 18:19:33 +0900

    week 2-1 ~step5 : create topAppBar

--- week 2-1-Layouts in JetpackCompose/week 2-1/app/src/main/java/com/example/composecodelabweek2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/week 2-1/app/src/main/java/com/example/composecodelabweek2_1/MainActivity.kt
@@ -12,6 +12,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BarChart
+import androidx.compose.material.icons.filled.Facebook
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.TravelExplore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
@@ -24,58 +29,75 @@ import androidx.compose.ui.unit.dp
 import com.example.composecodelabweek2_1.ui.theme.ComposeCodelabWeek21Theme
 
 //WEEK 2-1 : LAYOUTS CODE LAB
+//WHAT'S NEW : Scaffold API
+
+    //it is the most high-level composable
+    //provides slots for the most common top-level Material components such as TopAppBar, BottomAppBar, FloatingActionButton and Drawer.
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCodelabWeek21Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    PhotographerCard()
+                LayoutsCodelab()
+            }
+        }
+    }
+}
+
+@Composable
+fun LayoutsCodelab() {
+    Scaffold(     //Scaffold has a slot for a top AppBar
+        topBar = {
+            // 이 안에서 자유롭게 we can fill the slot with any composable we want
+            TopAppBar(  //app bar 넣기
+                //TopAppBar composable has slots for a title, navigation icon, actions.)
+                title = {
+                    Text(text = "Layouts Codelab")
+                },
+                actions = {  //normally actions modify the state of your app
+                    Row {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(Icons.Filled.Facebook, contentDescription = null)  // 아이콘 넣기
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(Icons.Filled.TravelExplore, contentDescription = null)
+                        }
+
+                    }
                 }
-            }
+
+            )
+
+//            Text(  //TopAppBar 말고 topBar 안에 이렇게 아무거나 넣을 수도 있음
+//                text = "LayoutsCodelab",
+//                style = MaterialTheme.typography.h3
+//            )
+
         }
+    ) { innerPadding ->                  //람다가 innerPadding 을 인자로 받고
+        BodyContent(modifier = Modifier
+            .padding(innerPadding)       //이 innerPadding 을 root composable에 한번에 적용
+            .padding(8.dp))              // + bodyContent 에 padding 추가로 적용
     }
 }
 
+//함수 쪼개기
+//새로운 composable 함수를 만들 때 아래와 같이 default Modifier 을 modifier 인자로 갖는게 좋음 -> reusable composable
 @Composable
-fun PhotographerCard(modifier: Modifier = Modifier) {    //함수의 인자로 empty modifier 를 주면 -> more flexible (enable the caller to modify composables) (이해???)
-    Row(modifier                            //이 함수의 root composable 의 인자로 넣기
-        .padding(8.dp)                      //래플에 포함 안되는 패딩 범위
-        .clip(RoundedCornerShape(4.dp))
-        .background(MaterialTheme.colors.surface)
-        .clickable(onClick = {  })          //modifier chaining 할 때 순서도 상관이 있음
-        .padding(16.dp)                     //(clickable 설정 후 패딩 주면 패딩 범위까지 모두 래플. 그 반대 순서면 패딩 범위는 래플에 포함x)
-    ) {
-        Surface (
-            modifier = Modifier.size(50.dp),
-            shape = CircleShape,
-            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-        ) {
-            //이미지 위치
-
-        }
-        Column (
-            modifier = Modifier                     //modifier 는 () 안에 위치하는 것임
-                .padding(start = 8.dp)              //왼쪽에 패딩 주는 방법
-                .align(Alignment.CenterVertically)  //center vertical!!
-            ){
-            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)  //text는 따로 modifier x
-            //LocalContentAlpha -> 자식의 투명도 조절 가능
-            //CompositionLocalProvider 를 통해 데이터 전달해서 ContentAlpha.medium 에 접근
-            CompositionLocalProvider(LocalContentAlpha provides  ContentAlpha.medium) { //medium 의 투명도 지정. (disabled는 더 옅어짐)
-                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
-            }
-
-        }
+fun BodyContent(modifier: Modifier = Modifier) {  //modifier 을 인자로 함
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "I'm excited for going through the Layouts codelab")
     }
 }
 
-@Preview(showBackground = true)
+
+@Preview
 @Composable
-fun PhotographerCardPreview() {
+fun LayoutsCodelabPreview() {
     ComposeCodelabWeek21Theme {
-        PhotographerCard()
+        LayoutsCodelab()
     }
 }
