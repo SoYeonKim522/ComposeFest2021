66d2c340133975e9a31b13c437bd73a7a2451346
SoYeonKim522 <sykim096@gmail.com>
2021-11-23 23:06:20 +0900

    week 2-2 ~step 7

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -19,12 +19,7 @@
 package com.codelabs.state.todo
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
@@ -35,6 +30,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -57,11 +53,6 @@ import kotlin.random.Random
  *   state 가 바뀌면 TodoScreen 가 다시 호출  with the new items and it can display them on screen
 
  * TodoScreen 을 ViewModel 과 연결시켜줘야 함 (그래서 버튼 이벤트 등 동작) (연결 매개체 : TodoActivityScreen)
- */
-
-
-/**
- * What's new : using (composables functions 가 가지고 있는) memory to add state to a composable.
  */
 
 
@@ -110,43 +101,59 @@ fun TodoScreen(
 
 //EditText(=text field) 추가
 //This is child composable of TodoItemInput
-@Composable
-fun TodoInputTextField(text: String, onTextChange: (String) -> Unit, modifier: Modifier) { //add a value(text) and onValueChange(onTextChange) parameter
-//    val (text, setText) = remember {  // -> to remember itself
-//        mutableStateOf("")
-//    }
-//    TodoInputText(text, setText, modifier)
-    TodoInputText(text, onTextChange, modifier)
-}
+//@Composable
+//fun TodoInputTextField(text: String, onTextChange: (String) -> Unit, modifier: Modifier) { //add a value(text) and onValueChange(onTextChange) parameter
+////    val (text, setText) = remember {  // -> to remember itself
+////        mutableStateOf("")
+////    }
+////    TodoInputText(text, setText, modifier)
+//    TodoInputText(text, onTextChange, modifier)
+//}
+
 
 //This is parent composable of TodoInputTextField
 @Composable
 fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {  //이벤트(onItemComplete)를 인자로 함.
     // onItemComplete is an event will fire when an item (TodoItem) is completed by the user
 
-    val (text, setText) = remember { mutableStateOf("") }  //원래 TodoInputTextField 에 있던 부분인데 부모 안으로 이동
+    val (text, setText) = remember { mutableStateOf("") }  //원래 TodoInputTextField 에 있던 state 인데 부모 안으로 이동
+    val (icon, setIcon) = remember { mutableStateOf(TodoIcon.Default)}  //icon 관련 state
+    val iconsVisible = text.isNotBlank()   //text 에 따라 달라지는 요소 (TodoItemInput 의 state 는 아님)
+    val submit = {
+        onItemComplete(TodoItem(text, icon))
+        setIcon(TodoIcon.Default)
+        setText("")
+    }
 
     Column {
         Row(Modifier
-            .padding(horizontal = 16.dp)
-            .padding(top = 16.dp)
+                .padding(horizontal = 16.dp)
+                .padding(top = 16.dp)
         ) {
-            TodoInputTextField(
-                text = text,            //추가
-                onTextChange = setText, //추가
+            TodoInputText(
+                text = text,
+                onTextChange = setText,
                 modifier = Modifier
-                .weight(1f)
-                .padding(end = 8.dp)
+                    .weight(1f)
+                    .padding(end = 8.dp),
+                    onImeAction = submit   //pass the submit callback to TodoInputText
             )
             TodoEditButton(
-                onClick = {
-                    onItemComplete(TodoItem(text))   // send onItemComplete event up
-                    setText("")        // clear the internal text
-                },
+                onClick = submit,    //pass the submit callback to TodoEditButton
+                //세줄지움
+//                onItemComplete(TodoItem(text))   // send onItemComplete event up
+//                setText("")        // clear the internal text
+//                setIcon(TodoIcon.Default)  //-> use ICON state in the onclick listener
+//                ,
                 text = "Add",
                 modifier = Modifier.align(Alignment.CenterVertically),
                 enabled = text.isNotBlank()    // enable if text is not blank
             )
+        }
+        if (iconsVisible) {
+            AnimatedIconRow(icon, setIcon, Modifier.padding(top = 8.dp))
+        } else {
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
@@ -163,8 +170,8 @@ fun TodoRow(
     todo: TodoItem,
     onItemClicked: (TodoItem) -> Unit,
     modifier: Modifier = Modifier,
-    iconAlpha : Float = remember(todo.id) { randomTint() }
-) {  //인자 위치로 이동 -> 이걸 적용할지 안할지 바로 컨트롤 가능
+    iconAlpha : Float = remember(todo.id) { randomTint() }  //인자 위치로 이동 -> 이걸 적용할지 안할지 바로 컨트롤 가능
+) {
     Row(
         modifier
             .clickable { onItemClicked(todo) }
