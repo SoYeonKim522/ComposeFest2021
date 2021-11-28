0fd24a43acc67097fae640c82c18e8693df6b912
SoYeonKim522 <sykim096@gmail.com>
2021-11-28 13:59:34 +0900

    week 4-2 ~step 9

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -21,6 +21,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -104,6 +105,15 @@ class PlantDetailFragment : Fragment() {
                         true
                     }
                     else -> false
+                }
+            }
+
+            //onCreateView 안에 추가
+            composeView.setContent {
+                //Compose world from here!!
+                MaterialTheme {
+                    PlantDetailDescription(plantDetailViewModel = plantDetailViewModel)
+
                 }
             }
         }
